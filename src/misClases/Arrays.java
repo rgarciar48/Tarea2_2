4b95247906f572b154fbc0b2c0f57e221a0e9688
Rubén García Recio <rgarciar48@educarex.es>
2023-12-06 16:04:25 +0100

    Segunda Fase

--- src/misClases/Arrays.java
+++ src/misClases/Arrays.java
@@ -1,5 +1,31 @@
 package misClases;
 
-public class Arrays {
+import javax.swing.JOptionPane;
 
-}
+public class Arrays {
+	
+    int[] vector;
+    
+    public Arrays(int numero) {
+        
+      vector=new int[numero];
+        
+      for(int i=0;i<numero;i++){
+            
+         int valor=Integer.parseInt(JOptionPane.showInputDialog("Introduce valor"));
+         vector[i]=valor;
+         }
+     }
+    
+    public void getDatosArray() {
+        
+        System.out.println("Los valores de la matriz son: ");
+        
+        for(int j=0;j<vector.length;j++){
+            
+            System.out.print(vector[j] + "; ");
+        }
+    
+    }
+         }
+
